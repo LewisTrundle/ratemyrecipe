59430260566cd843528c86b2625bd95f73cc4df2
Lewis Trundle <2469635T@student.gla.ac.uk>
2021-03-12 15:11:13 +0000

    Added stuff to views

--- ratemyrecipeapp/urls.py
+++ ratemyrecipeapp/urls.py
@@ -6,6 +6,10 @@ app_name = 'ratemyrecipeapp'
 urlpatterns = [
         # If an empty string is provided as the url, this is mapped to the index page
         path('', views.index, name='index'),
+        
         path('categories/', views.categories, name="categories"),
+        path('trending/', views.trending, name="trending"),
+        path('signup/', views.sign_up, name="signup"),
+        path('login/', views.login, name="login"),
 ]
 
--- ratemyrecipeapp/views.py
+++ ratemyrecipeapp/views.py
@@ -1,10 +1,23 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from ratemyrecipeapp.models import Category, Recipe, Rating
+from math import random
 
 def index(request):
-    # This dictionary is passed to the template engine
+    
+    # Gets a list of every recipe
+    recipes = list(Recipe.objects.all())
+    # Picks a random recipe
+    ran_recipe = random.sample(recipes, 1)
+    
+    rating = Rating.objects.filter(recipe=ran_recipe)
+    
+    
+    
     context_dict = {}
     context_dict['welcome'] = "Welcome to Rate My Recipe!"
+    context_dict['ran_recipe'] = ran_recipe
+    context_dict['rating'] = rating
     
     return render(request, 'rango/index.html', context=context_dict)
 
@@ -12,3 +25,23 @@ def index(request):
 
 def categories(request):
     return HttpResponse("This is the categories page")
+
+
+def trending(request):
+    pass
+
+def sign_up(request):
+    pass
+
+def login(request):
+    pass
+
+def my_account(request):
+    pass
+
+def my_recipes(request):
+    pass
+
+def recipes_ive_rated(request):
+    pass
+
