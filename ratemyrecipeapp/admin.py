3939396b96f0729b3a54c8595cae6d42a5cf6350
Sarahi Villegas <2477179V@student.gla.ac.uk>
2021-03-12 13:08:04 +0000

    added rating model, modified recipe model

--- ratemyrecipeapp/admin.py
+++ ratemyrecipeapp/admin.py
@@ -1,4 +1,7 @@
 from django.contrib import admin
+from ratemyrecipeapp.models import Category, Recipe
 from ratemyrecipeapp.models import UserProfile
 
+admin.site.register(Category)
+admin.site.register(Recipe)
 admin.site.register(UserProfile)
--- ratemyrecipeapp/models.py
+++ ratemyrecipeapp/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.core.validators import MaxValueValidator, MinValueValidator
 
 
 class UserProfile(models.Model):
@@ -14,6 +15,9 @@ class UserProfile(models.Model):
 class Category(models.Model):
     name = models.CharField(max_length=30, unique=True)
 
+    class Meta:
+        verbose_name_plural = 'Categories'
+
     def __str__(self):
         return self.name
 
@@ -26,7 +30,7 @@ class Recipe(models.Model):
     directions = models.TextField(max_length=1000)
     is_vegan = models.BooleanField()
     cost = models.PositiveSmallIntegerField()
-    time_needed = models.DurationField()
+    time_needed = models.IntegerField(help_text='HH:MM:SS format')
 
     added_by = models.ForeignKey(UserProfile, on_delete=models.CASCADE)
 
@@ -34,4 +38,12 @@ class Recipe(models.Model):
         return f'{self.title} by {self.added_by}'
 
 
-# rating needs a many to many relationship, check documentation
+class Rating(models.Model):
+    rating = models.IntegerField(
+        validators=[MinValueValidator(1), MaxValueValidator(5)]
+    )
+    recipe = models.ForeignKey(Recipe, on_delete=models.CASCADE)
+    rated_by = models.ForeignKey(UserProfile, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f'Rating for {self.recipe} by {self.rated_by}'
