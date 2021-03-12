8f4a059efbbc5a346f5b523ccb4b941aedd7681b
Sarahi Villegas <2477179V@student.gla.ac.uk>
2021-03-12 08:12:32 +0000

    models for category, recipe and user created. made first migration

--- ratemyrecipeapp/models.py
+++ ratemyrecipeapp/models.py
@@ -1,3 +1,37 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class UserProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+
+    # I think we're not adding any other attributes like web or picture?
+
+    def __str__(self):
+        return self.user.username
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=30, unique=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Recipe(models.Model):
+    title = models.CharField(max_length=100)  # does it have to be unique?
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+
+    ingredients = models.TextField(max_length=1000)
+    directions = models.TextField(max_length=1000)
+    is_vegan = models.BooleanField()
+    cost = models.PositiveSmallIntegerField()
+    time_needed = models.DurationField()
+
+    added_by = models.ForeignKey(UserProfile, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f'{self.title} by {self.added_by}'
+
+
+# rating needs a many to many relationship, check documentation
